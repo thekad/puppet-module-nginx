ab870cbce4b6b42941171ae36a582ab1fd8f2a96
Jorge A Gallegos <kad@blegh.net>
2011-10-02 16:37:02 -0700

    Adding a simple nginx::config define

---
+++ manifests/config.pp
@@ -0,0 +1,48 @@
+# -*- mode: puppet; sh-basic-offset: 4; indent-tabs-mode: nil; coding: utf-8 -*-
+# vim: tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8
+
+define nginx::config($config='', $content='', $source='', $order='500', $ensure='present') {
+
+    if (($content and $source) or (!$content and !$source)){
+        err('Must define "content" or "source" for nginx::config!')
+        fail('Must define "content" or "source" for nginx::config!')
+    }
+
+    case $ensure {
+        'present': {
+            $ensure_real = 'present'
+        }
+        'absent', 'purged': {
+            $ensure_real = 'purged'
+        }
+        default: {
+            err("Invalid value for 'ensure' (valid values: present, purged, absent): $ensure")
+            fail("Invalid value for 'ensure': $ensure")
+        }
+    }
+
+    $config_name = $config ? {
+        ''      => $name,
+        default => $config,
+    }
+
+    File {
+        require => File['/etc/nginx/conf.d'],
+        mode    => 0644,
+        notify  => Exec['nginx::reload'],
+    }
+
+    file {
+        "/etc/nginx/conf.d/${order}-${config_name}.conf":
+            ensure  => $ensure_real,
+            content => $content ? {
+                ''      => undef,
+                default => $content,
+            },
+            source  => $source ? {
+                ''      => undef,
+                default => $source,
+            };
+    }
+}
+
