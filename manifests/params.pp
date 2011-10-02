a5d0389610028f52b13010617c4f3ea9dc9c3f4d
Jorge A Gallegos <kad@blegh.net>
2011-10-02 15:31:07 -0700

    Adding run user/group. Reload/upgrade commands

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,10 +2,17 @@ class nginx::params {
 
     case $operatingsystem {
         'ubuntu', 'debian': {
-            $run_as = 'www-data'
+            $run_as_user = 'www-data'
+            $run_as_group = 'adm'
+            $reload_cmd = '/usr/sbin/service nginx reload'
+#           Ubuntu doesn't support upgrade in place
+            $upgrade_cmd = '/bin/false'
         }
         'redhat', 'centos', 'fedora': {
-            $run_as = 'nginx'
+            $run_as_user = 'nginx'
+            $run_as_group = 'root'
+            $reload_cmd = '/sbin/service nginx reload'
+            $upgrade_cmd = '/sbin/service nginx upgrade'
         }
     }
 }
